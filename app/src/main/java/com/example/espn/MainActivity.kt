0897fb61c88ec6b348ef7138ffdb3a53456edca1
jagmohankrrai <jagmohankrrai@gmail.com>
2023-02-03 15:28:52 +0530

    https://github.com/jagmohankrrai/ENSP.git

--- app/src/main/java/com/example/espn/MainActivity.kt
+++ app/src/main/java/com/example/espn/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.espn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNav = findViewById(R.id.BottomNavBar)
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home->{
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.calendar->{
+                    loadFragment(TournmentFragment())
+                    true
+                }
+                R.id.more-> {
+                    loadFragment(LiveFragment())
+                    true
+                }
+                else -> {false}
+            }
+//            else false
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
     }
 }
